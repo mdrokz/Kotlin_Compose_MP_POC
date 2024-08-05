714d6fe94f5366832d63c3a47af511046a51b906
mdrokz <mohammadmunshi@gmail.com>
2024-08-05 16:14:27 +0530

    feat: add support for parsing images in IOS

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -30,6 +30,10 @@ kotlin {
     }
     
     sourceSets {
+
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
         
         androidMain.dependencies {
             implementation(compose.preview)
@@ -89,5 +93,7 @@ android {
 }
 dependencies {
     implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.ui.graphics.android)
+    implementation(libs.androidx.ui.graphics.desktop)
 }
 
--- composeApp/src/iosMain/kotlin/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/Platform.ios.kt
@@ -1,4 +1,14 @@
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.toComposeImageBitmap
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
+import org.jetbrains.skia.Image
 import platform.UIKit.UIDevice
 
 class IOSPlatform: Platform {
@@ -6,6 +16,25 @@ class IOSPlatform: Platform {
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+fun ByteArray.toImageBitmap(): ImageBitmap =
+    Image.makeFromEncoded(this).toComposeImageBitmap()
+
+@OptIn(ExperimentalForeignApi::class, BetaInteropApi::class)
 actual suspend fun loadNetworkImage1(link: String): ImageBitmap {
-    TODO("Not yet implemented")
+    val client = HttpClient()
+
+    return withContext(Dispatchers.IO) {
+        try {
+            val response = client.get(link)
+            val bytes = response.body<ByteArray>()
+
+            val bitmap = bytes.toImageBitmap()
+            bitmap
+        } catch (e: Exception) {
+            throw e
+        } finally {
+            client.close()
+        }
+    }
 }
